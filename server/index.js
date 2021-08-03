4ea385a5f7bd633fdf1c213107f59f0b935d0f7e
Cas <51390039+CasColucci@users.noreply.github.com>
2021-08-03 14:16:43 -0400

    Updated to socket.io
    Now using socket.io rather than just websockets. Seems to be working fine but I'll need to do more research.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
+const http = require("http").createServer();
+const io = require("socket.io")(http, {
+  cors: { origin: "*" },
+});
 
-const socket = new WebSocket.Server({ port: 8080 });
+//const socket = new WebSocket.Server({ port: 8080 });
 
+io.on("connection", (socket) => {
+  console.log("New user connected");
+
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", `${socket.id.substr(0, 2)} said ${message}`);
+  });
+});
+
+/*
 socket.on("connection", ws => {
     console.log("New client connected");
 
@@ -9,3 +23,5 @@ socket.on("connection", ws => {
         console.log("Client has disconnected");
     });
 });
+*/
+http.listen(8080, () => console.log("listening on http://localhost:8080"));
