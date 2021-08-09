d75119dfb489cff178232e583c8a5236b94af049
Cas <51390039+CasColucci@users.noreply.github.com>
2021-08-09 17:52:51 -0400

    Beginning client side, putting down for today

--- public/app.js
+++ public/app.js
@@ -1,1 +1,21 @@
 import React, { useState, useRef, useEffect } from "react";
+import io from "socket.io/client";
+import immer from "immer";
+
+function App() {
+  const [username, setusername] = useState("");
+  const [connected, setconnected] = useState(false);
+  const [allusers, setallusers] = useState([]);
+  const [allcharacters, setallcharacters] = useState([]);
+  const socketref = useRef();
+
+  let body;
+  if (connected) {
+    body = <userlist user={user} users={users} />;
+  } else {
+    body = <form></form>;
+  }
+  return <div className="App">{body}</div>;
+}
+
+export default App;
