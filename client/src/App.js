843828471643c31399da02dac9d7b4e9ec701d10
Cas <51390039+CasColucci@users.noreply.github.com>
2021-08-13 13:34:42 -0400

    React setup for frontend

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { useState, useRef, useEffect } from "react";
+import io from "socket.io/client";
+import immer from "immer";
+import Form from "./components/UserForm";
+
+function App() {
+  const [username, setusername] = useState("");
+  const [connected, setconnected] = useState(false);
+  const [allusers, setallusers] = useState([]);
+  const [allcharacters, setallcharacters] = useState([]);
+  const socketref = useRef();
+
+  let body;
+  if (connected) {
+    body = <userlist user={user} users={users} />;
+  } else {
+    body = (
+      <Form username={username} onChange={handlechange} connect={connect} />
+    );
+  }
+  return <div className="App">{body}</div>;
+}
+
+export default App;
