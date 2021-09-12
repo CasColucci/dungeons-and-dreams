3d569966636a7b0d280a67e8c582a5823e06c4e8
Cas <51390039+CasColucci@users.noreply.github.com>
2021-09-11 22:40:15 -0400

    Been at this for a while but finally making progress

--- front/index.js
+++ front/index.js
@@ -1,5 +1,40 @@
 const socket = io("http://localhost:3000");
 
-function init() {}
+const joinBtn = document.getElementById("joinBtn");
+const createBtn = document.getElementById("createBtn");
+const roomIdInput = document.getElementById("roomIdInput");
 
-init();
+const roomScreen = document.getElementById("roomScreen");
+const initialScreen = document.getElementById("initialScreen");
+const roomIdDisplay = document.getElementById("roomIdDisplay");
+
+createBtn.addEventListener("click", newRoom);
+joinBtn.addEventListener("click", joinRoom);
+
+socket.on("init", handleInit);
+socket.on("roomId", handleRoomId);
+
+let userNumber;
+
+function newRoom() {
+  socket.emit("newRoom");
+  setup();
+}
+
+function joinRoom() {
+  const code = roomIdInput.value;
+  socket.emit("joinRoom", code);
+  setup();
+}
+
+function handleInit(number) {
+  userNumber = number;
+}
+function handleRoomId(roomId) {
+  roomIdDisplay.innerText = roomId;
+}
+
+function setup() {
+  initialScreen.style.display = "none";
+  roomScreen.style.display = "block";
+}
--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const { makeid } = require("./utils");
+
+const socketRooms = {};
 
 app.use(express.static(path.join(__dirname, "../front")));
 
@@ -17,6 +20,16 @@ let users = [];
 
 io.on("connection", (socket) => {
   console.log("User has connected");
+  socket.on("newRoom", handleNewRoom);
 
   socket.on("disconnect", (socket) => console.log("User disconnected"));
+
+  function handleNewRoom() {
+    let roomName = makeid(5);
+    socketRooms[socket.id] = roomName;
+    socket.emit("roomId", roomName);
+    socket.join(roomName);
+    socket.number = 1;
+    socket.emit("init", 1);
+  }
 });
